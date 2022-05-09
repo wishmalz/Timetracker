57abd57f6d9cd09996ec2f20ea0ac8667731a8d9
Valerii Bakalenko <valeriy.bakalenko@gmail.com>
2022-05-09 17:04:20 +0300

    FEATURE
    clients entity added fields

--- src/client/entities/client.entity.ts
+++ src/client/entities/client.entity.ts
@@ -1,1 +1,13 @@
-export class Client {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'clients' })
+export class Client {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  name: string;
+
+  @Column({ nullable: true })
+  avatar: string;
+}
