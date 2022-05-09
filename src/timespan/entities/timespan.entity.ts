d426a8d9a1532420ed350904cb24a0c9da5cdd56
Valerii Bakalenko <valeriy.bakalenko@gmail.com>
2022-05-09 17:19:05 +0300

    FEATURE
    timespan added fields

--- src/timespan/entities/timespan.entity.ts
+++ src/timespan/entities/timespan.entity.ts
@@ -1,1 +1,41 @@
-export class Timespan {}
+import {
+  BeforeUpdate,
+  Column,
+  Double,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'timespans' })
+export class Timespan {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  task_name: string;
+
+  @Column({ default: '' })
+  description: string;
+
+  @Column()
+  duration: Double;
+
+  @Column({ type: 'timestamp' })
+  start_time: Date;
+
+  @Column({ type: 'timestamp' })
+  end_time: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updated_at: Date;
+
+  @BeforeUpdate()
+  updateTimestamp() {
+    this.updated_at = new Date();
+  }
+  //   user_id
+  // project_id
+}
